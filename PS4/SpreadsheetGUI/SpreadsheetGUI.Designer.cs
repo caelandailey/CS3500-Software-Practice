e9d4b9527facd3eeff7a12d8b68fc5762d747dcd
Karina <Karina@DESKTOP-D4K9NKC>
2016-10-26 22:45:43 -0600

    tried to set up some more

--- PS4/SpreadsheetGUI/SpreadsheetGUI.Designer.cs
+++ PS4/SpreadsheetGUI/SpreadsheetGUI.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace SpreadsheetGUI
+﻿namespace SS
 {
     partial class SpreadsheetGUI
     {
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.spreadsheetPanel1 = new SS.SpreadsheetPanel();
+            this.File = new System.Windows.Forms.ListBox();
+            this.cellName = new System.Windows.Forms.TextBox();
+            this.cellValue = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // spreadsheetPanel1
+            // 
+            this.spreadsheetPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.spreadsheetPanel1.Location = new System.Drawing.Point(1, 26);
+            this.spreadsheetPanel1.Name = "spreadsheetPanel1";
+            this.spreadsheetPanel1.Size = new System.Drawing.Size(613, 344);
+            this.spreadsheetPanel1.TabIndex = 0;
+            // 
+            // File
+            // 
+            this.File.FormattingEnabled = true;
+            this.File.Items.AddRange(new object[] {
+            "Open ",
+            "Save",
+            "Close"});
+            this.File.Location = new System.Drawing.Point(1, -3);
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(53, 30);
+            this.File.TabIndex = 1;
+            // 
+            // cellName
+            // 
+            this.cellName.Location = new System.Drawing.Point(111, 0);
+            this.cellName.Name = "cellName";
+            this.cellName.Size = new System.Drawing.Size(46, 20);
+            this.cellName.TabIndex = 2;
+            this.cellName.Text = "A1:";
+            // 
+            // cellValue
+            // 
+            this.cellValue.Location = new System.Drawing.Point(154, 0);
+            this.cellValue.Name = "cellValue";
+            this.cellValue.Size = new System.Drawing.Size(123, 20);
+            this.cellValue.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(274, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(489, 0);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(61, 23);
+            this.enterButton.TabIndex = 5;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            // 
+            // SpreadsheetGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 382);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cellValue);
+            this.Controls.Add(this.cellName);
+            this.Controls.Add(this.File);
+            this.Controls.Add(this.spreadsheetPanel1);
+            this.Name = "SpreadsheetGUI";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private SS.SpreadsheetPanel spreadsheetPanel1;
+        private System.Windows.Forms.ListBox File;
+        private System.Windows.Forms.TextBox cellName;
+        private System.Windows.Forms.TextBox cellValue;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button enterButton;
     }
 }
 
--- PS4/SpreadsheetGUI/SpreadsheetGUI.cs
+++ PS4/SpreadsheetGUI/SpreadsheetGUI.cs
@@ -8,13 +8,52 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SpreadsheetGUI
+
+namespace SS
 {
     public partial class SpreadsheetGUI : Form
     {
         public SpreadsheetGUI()
         {
             InitializeComponent();
+
+            spreadsheetPanel1.SelectionChanged += displaySelection;
+            spreadsheetPanel1.SelectionChanged += displayValue;
+          
         }
+
+        /// <summary>
+        /// Gets the value of the cell selected
+        /// </summary>
+        /// <param name="ss"></param>
+        private void displayValue(SpreadsheetPanel ss)
+        {
+            int row, col;
+            String value;
+            ss.GetSelection(out col, out row);
+            ss.GetValue(col, row, out value);
+            if (value == "")
+            {
+                ss.SetValue(col, row, DateTime.Now.ToLocalTime().ToString("T"));
+                ss.GetValue(col, row, out value);
+                MessageBox.Show("Selection: column " + col + " row " + row + " value " + value);
+            }
+            cellValue.Text = value;
+        }
+
+        /// <summary>
+        /// Shows the cell's name selected
+        /// </summary>
+        /// <param name="ss"></param>
+        private void displaySelection(SpreadsheetPanel ss)
+        {
+            int row, col;
+            ss.GetSelection(out col, out row);
+            col += 64;
+            cellName.Text = (char)col + row + ":";
+        }
+
+        
+
     }
 }
